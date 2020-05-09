b9c46059717ba11ac020e742a0c4ea67b3eba031
rcpd <rcpd22@gmail.com>
2020-05-09 10:54:12 +1000

    Add HDL solutions

--- hdl.py
+++ hdl.py
@@ -172,6 +172,14 @@ class NandGate(Gate):
 class NotGate(Gate):
     """
     For a single input, return the opposite
+
+    CHIP Not {
+    IN in;
+    OUT out;
+
+    PARTS:
+    Nand(a=in, b=in, out=out);
+    }
     """
     def calculate(self):
         """
@@ -185,6 +193,28 @@ class NotGate(Gate):
 class Not16Gate(Gate):
     """
     NotGate but with two x 16 bit inputs and one 16 bit output, each bit is compared across both inputs
+    CHIP Not16 {
+    IN in[16];
+    OUT out[16];
+
+    PARTS:
+    Not(in=in[0], out=out[0]);
+    Not(in=in[1], out=out[1]);
+    Not(in=in[2], out=out[2]);
+    Not(in=in[3], out=out[3]);
+    Not(in=in[4], out=out[4]);
+    Not(in=in[5], out=out[5]);
+    Not(in=in[6], out=out[6]);
+    Not(in=in[7], out=out[7]);
+    Not(in=in[8], out=out[8]);
+    Not(in=in[9], out=out[9]);
+    Not(in=in[10], out=out[10]);
+    Not(in=in[11], out=out[11]);
+    Not(in=in[12], out=out[12]);
+    Not(in=in[13], out=out[13]);
+    Not(in=in[14], out=out[14]);
+    Not(in=in[15], out=out[15]);
+    }
     """
     def calculate(self):
         byte_str = "0b"
@@ -199,6 +229,15 @@ class Not16Gate(Gate):
 class AndGate(Gate):
     """
     For two 1 inputs return a 1 output, else return a 0 output
+
+    CHIP And {
+    IN a, b;
+    OUT out;
+
+    PARTS:
+    Nand(a=a, b=b, out=aNand);
+    Nand(a=aNand, b=aNand, out=out);
+    }
     """
     def calculate(self):
         """
@@ -212,6 +251,15 @@ class AndGate(Gate):
 class And16Gate(Gate):
     """
     AndGate but with two x 16 bit inputs and one 16 bit output, each bit is compared across both inputs
+
+    CHIP And {
+    IN a, b;
+    OUT out;
+
+    PARTS:
+    Nand(a=a, b=b, out=aNand);
+    Nand(a=aNand, b=aNand, out=out);
+    }
     """
     def calculate(self):
         byte_str = "0b"
@@ -226,6 +274,16 @@ class And16Gate(Gate):
 class OrGate(Gate):
     """
     If either of the two inputs are 1 return a 1 output, else return a 0 output
+
+    CHIP Or {
+    IN a, b;
+    OUT out;
+
+    PARTS:
+    Nand(a=a, b=a, out=aNand);
+    Nand(a=b, b=b, out=bNand);
+    Nand(a=aNand, b=bNand, out=out);
+    }
     """
     def calculate(self):
         # endianness does not matter as only 1 bit returned
@@ -237,6 +295,29 @@ class OrGate(Gate):
 class Or16Gate(Gate):
     """
     OrGate but with two x 16 bit inputs and one 16 bit output, each bit is compared across both inputs
+
+    CHIP Or16 {
+    IN a[16], b[16];
+    OUT out[16];
+
+    PARTS:
+    Or(a=a[0], b=b[0], out=out[0]);
+    Or(a=a[1], b=b[1], out=out[1]);
+    Or(a=a[2], b=b[2], out=out[2]);
+    Or(a=a[3], b=b[3], out=out[3]);
+    Or(a=a[4], b=b[4], out=out[4]);
+    Or(a=a[5], b=b[5], out=out[5]);
+    Or(a=a[6], b=b[6], out=out[6]);
+    Or(a=a[7], b=b[7], out=out[7]);
+    Or(a=a[8], b=b[8], out=out[8]);
+    Or(a=a[9], b=b[9], out=out[9]);
+    Or(a=a[10], b=b[10], out=out[10]);
+    Or(a=a[11], b=b[11], out=out[11]);
+    Or(a=a[12], b=b[12], out=out[12]);
+    Or(a=a[13], b=b[13], out=out[13]);
+    Or(a=a[14], b=b[14], out=out[14]);
+    Or(a=a[15], b=b[15], out=out[15]);
+    }
     """
     def calculate(self):
         byte_str = "0b"
@@ -248,9 +329,23 @@ class Or16Gate(Gate):
         return byte_str
 
 
-class OrGate8Way(Gate):
+class Or8Way(Gate):
     """
     8 bit bus of 1 bit inputs, 1 bit output, if any bits 1 return 1, else 0
+
+    CHIP Or8Way {
+    IN in[8];
+    OUT out;
+
+    PARTS:
+    Or(a=in[0], b=in[1], out=or1);
+    Or(a=in[2], b=in[3], out=or2);
+    Or(a=in[4], b=in[5], out=or3);
+    Or(a=in[6], b=in[7], out=or4);
+    Or(a=or1, b=or2, out=or5);
+    Or(a=or3, b=or4, out=or6);
+    Or(a=or5, b=or6, out=out);
+    }
     """
     def calculate(self):
         # endianness does not matter as only 1 bit returned
@@ -268,6 +363,17 @@ class OrGate8Way(Gate):
 class XorGate(Gate):
     """
     If the two inputs are different return a 1 output, else return a 0 output
+
+    CHIP Xor {
+    IN a, b;
+    OUT out;
+
+    PARTS:
+    Nand(a=a, b=b, out=aNand);
+    Nand(a=a, b=aNand, out=bNand);
+    Nand(a=aNand, b=b, out=cNand);
+    Nand(a=bNand, b=cNand, out=out);
+    }
     """
     def calculate(self):
         # endianness does not matter as only 1 bit returned
@@ -298,6 +404,17 @@ class XNorGate(Gate):
 class Mux(Gate):
     """
     Select an output from two inputs, only chosen input will be emitted
+
+    CHIP Mux {
+    IN a, b, sel;
+    OUT out;
+
+    PARTS:
+    Nand(a=sel, b=sel, out=aNand);
+    Nand(a=b, b=sel, out=bNand);
+    Nand(a=aNand, b=a, out=cNand);
+    Nand(a=bNand, b=cNand, out=out);
+    }
     """
     def calculate(self):
         # endianness does not matter as only 1 bit returned
@@ -310,6 +427,29 @@ class Mux(Gate):
 class Mux16(Gate):
     """
     Mux but with two x 16 bit inputs and one 16 bit output, only chosen input will be emitted
+
+    CHIP Mux16 {
+    IN a[16], b[16], sel;
+    OUT out[16];
+
+    PARTS:
+    Mux(a=a[0], b=b[0], sel=sel, out=out[0]);
+    Mux(a=a[1], b=b[1], sel=sel, out=out[1]);
+    Mux(a=a[2], b=b[2], sel=sel, out=out[2]);
+    Mux(a=a[3], b=b[3], sel=sel, out=out[3]);
+    Mux(a=a[4], b=b[4], sel=sel, out=out[4]);
+    Mux(a=a[5], b=b[5], sel=sel, out=out[5]);
+    Mux(a=a[6], b=b[6], sel=sel, out=out[6]);
+    Mux(a=a[7], b=b[7], sel=sel, out=out[7]);
+    Mux(a=a[8], b=b[8], sel=sel, out=out[8]);
+    Mux(a=a[9], b=b[9], sel=sel, out=out[9]);
+    Mux(a=a[10], b=b[10], sel=sel, out=out[10]);
+    Mux(a=a[11], b=b[11], sel=sel, out=out[11]);
+    Mux(a=a[12], b=b[12], sel=sel, out=out[12]);
+    Mux(a=a[13], b=b[13], sel=sel, out=out[13]);
+    Mux(a=a[14], b=b[14], sel=sel, out=out[14]);
+    Mux(a=a[15], b=b[15], sel=sel, out=out[15]);
+    }
     """
     def calculate(self):
         byte_str = "0b"
@@ -324,6 +464,16 @@ class Mux16(Gate):
 class Mux4Way16(Gate):
     """
     Mux16 but with 4 x 16 bit inputs, one 16 bit output, two bit selector, only selected is emitted
+
+    CHIP Mux4Way16 {
+    IN a[16], b[16], c[16], d[16], sel[2];
+    OUT out[16];
+
+    PARTS:
+    Mux16(a=a, b=b, sel=sel[0], out=muxAB);
+    Mux16(a=c, b=d, sel=sel[0], out=muxCD);
+    Mux16(a=muxAB, b=muxCD, sel=sel[1], out=out);
+    }
     """
     def calculate(self):
         # endianness only matters for selector / result order
@@ -335,6 +485,18 @@ class Mux4Way16(Gate):
 class Mux8Way16(Gate):
     """
     Mux16 but with 8 x 16 bit inputs, one 16 bit output, 3 bit selector, only selected is emitted
+
+    CHIP Mux8Way16 {
+    IN a[16], b[16], c[16], d[16],
+       e[16], f[16], g[16], h[16],
+       sel[3];
+    OUT out[16];
+
+    PARTS:
+    Mux4Way16(a=a, b=b, c=c, d=d, sel=sel[0..1], out=muxAD);
+    Mux4Way16(a=e, b=f, c=g, d=h, sel=sel[0..1], out=muxEH);
+    Mux16(a=muxAD, b=muxEH, sel=sel[2], out=out);
+    }
     """
     def calculate(self):
         # endianness only matters for selector / result order
@@ -346,6 +508,18 @@ class Mux8Way16(Gate):
 class DMux(Mux):
     """
     Select one of two outputs, input passes through and unselected output is always 0
+
+    CHIP DMux {
+    IN in, sel;
+    OUT a, b;
+
+    PARTS:
+    Nand(a=sel, b=sel, out=aNand);
+    Nand(a=in, b=aNand, out=bNand);
+    Nand(a=sel, b=in, out=cNand);
+    Nand(a=bNand, b=bNand, out=a);
+    Nand(a=cNand, b=cNand, out=b);
+    }
     """
     def calculate(self):
         # endianness does not matter as only 2 x 1 bit returned?
@@ -358,6 +532,16 @@ class DMux(Mux):
 class DMux4Way(DMux):
     """
     With a 2 bit selector choose one of four outputs, input passes through and unselected is always 0
+
+    CHIP DMux4Way {
+    IN in, sel[2];
+    OUT a, b, c, d;
+
+    PARTS:
+    DMux(in=in, sel=sel[1], a=dIn0, b=dIn1);
+    DMux(in=dIn0, sel=sel[0], a=a, b=b);
+    DMux(in=dIn1, sel=sel[0], a=c, b=d);
+    }
     """
     def calculate(self):
         # endianness only matters for selector / result order
@@ -370,6 +554,16 @@ class DMux4Way(DMux):
 class DMux8Way(DMux):
     """
     With a 3 bit selector choose one of 8 outputs, input passes through and unselected is always 0
+
+    CHIP DMux8Way {
+    IN in, sel[3];
+    OUT a, b, c, d, e, f, g, h;
+
+    PARTS:
+    DMux(in=in, sel=sel[2], a=dIn0, b=dIn1);
+    DMux4Way(in=dIn0, sel=sel[0..1], a=a, b=b, c=c, d=d);
+    DMux4Way(in=dIn1, sel=sel[0..1], a=e, b=f, c=g, d=h);
+    }
     """
     def calculate(self):
         # endianness only matters for selector / result order
@@ -382,6 +576,16 @@ class DMux8Way(DMux):
 class HalfAdder(Gate):
     """
     Computes the sum of 2 x 1 bit inputs, output carry bit & sum bit
+
+    CHIP HalfAdder {
+    IN a, b;    // 1-bit inputs
+    OUT sum,    // Right bit of a + b
+        carry;  // Left bit of a + b
+
+    PARTS:
+    Xor(a=a, b=b, out=sum);
+    And(a=a, b=b, out=carry);
+    }
     """
     def calculate(self):
         carry = AndGate().evaluate(a=self.a, b=self.b)
@@ -391,8 +595,8 @@ class HalfAdder(Gate):
 
 class FullAdder(Gate):
     """
-    Computes the sum of 3 x 1 bit inputs, output carry bit & sum bit
-        IN a, b, c;  // 1-bit inputs
+    CHIP FullAdder {
+    IN a, b, c;  // 1-bit inputs
     OUT sum,     // Right bit of a + b + c
         carry;   // Left bit of a + b + c
 
@@ -400,6 +604,7 @@ class FullAdder(Gate):
     HalfAdder(a=a, b=b, sum=sumAB, carry=carryAB);
     HalfAdder(a=c, b=sumAB, sum=sum, carry=carryABC);
     Or(a=carryABC, b=carryAB, out=carry);
+    }
 
     """
     def calculate(self):
@@ -408,6 +613,31 @@ class FullAdder(Gate):
         carry = OrGate().evaluate(a=carry_abc, b=carry_ab)
         return carry, _sum
 
+class Add16(Gate):
+    """
+    CHIP Add16 {
+        IN a[16], b[16];
+        OUT out[16];
+
+        PARTS:
+        HalfAdder(a=a[0], b=b[0], sum=out[0], carry=carry1);
+        FullAdder(a=a[1], b=b[1], c=carry1, sum=out[1], carry=carry2);
+        FullAdder(a=a[2], b=b[2], c=carry2, sum=out[2], carry=carry3);
+        FullAdder(a=a[3], b=b[3], c=carry3, sum=out[3], carry=carry4);
+        FullAdder(a=a[4], b=b[4], c=carry4, sum=out[4], carry=carry5);
+        FullAdder(a=a[5], b=b[5], c=carry5, sum=out[5], carry=carry6);
+        FullAdder(a=a[6], b=b[6], c=carry6, sum=out[6], carry=carry7);
+        FullAdder(a=a[7], b=b[7], c=carry7, sum=out[7], carry=carry8);
+        FullAdder(a=a[8], b=b[8], c=carry8, sum=out[8], carry=carry9);
+        FullAdder(a=a[9], b=b[9], c=carry9, sum=out[9], carry=carry10);
+        FullAdder(a=a[10], b=b[10], c=carry10, sum=out[10], carry=carry11);
+        FullAdder(a=a[11], b=b[11], c=carry11, sum=out[11], carry=carry12);
+        FullAdder(a=a[12], b=b[12], c=carry12, sum=out[12], carry=carry13);
+        FullAdder(a=a[13], b=b[13], c=carry13, sum=out[13], carry=carry14);
+        FullAdder(a=a[14], b=b[14], c=carry14, sum=out[14], carry=carry15);
+        FullAdder(a=a[15], b=b[15], c=carry15, sum=out[15], carry=carry16);
+    }
+    """
 
 def input_unit_test():
     """
@@ -457,7 +687,7 @@ def main():
     _and16 = And16Gate()
     _or = OrGate()
     _or16 = Or16Gate()
-    _or8way = OrGate8Way()
+    _or8way = Or8Way()
     _xor = XorGate()
     # TODO: nor, xnor
     _mux = Mux()
